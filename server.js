6b4e8fa0eaa9571a12108928c2ee80b8962b8970
text <leanhphq@gmail.com>
2026-05-30 19:59:25 +0700

    fix: login by Google

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const swaggerSpec = require("./config/swagger");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Swagger route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
