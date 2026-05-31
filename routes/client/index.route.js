5b7dfa4b43fcdf0b99834f76d7db0a9ff6c6b888
Nguyen0362 <nanhnguyen6789@gmail.com>
2026-05-31 09:18:44 +0700

    feat: Search for stores and nurseries

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,11 @@
 const authRoute = require('./auth.route');
 const myGardenRoute = require("./my_garden.route");
-const userRoute = require("./user.route")
+const userRoute = require("./user.route");
+const storeRoute = require("./store.route");
 
 module.exports = (app) => {
   app.use("/auth", authRoute);
   app.use("/my-garden", myGardenRoute);
-  app.use("/user", userRoute)
+  app.use("/user", userRoute);
+  app.use("/store", storeRoute);
 };
