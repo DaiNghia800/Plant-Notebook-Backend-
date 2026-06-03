320a7fd67941d210e2f5b763f49d53faa29ac319
Nguyen0362 <nanhnguyen6789@gmail.com>
2026-06-03 22:42:28 +0700

    Revert "Refactor/my garden fix"

--- config/firebase.js
+++ config/firebase.js
@@ -3,10 +3,9 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
-const messaging = admin.messaging();
 
-module.exports = { db, messaging };
+module.exports = db;
