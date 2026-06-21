891a6638a9fc1d619bdf5920414d1235aff06f7a
Bui Dai Nghia <6451071050@st.utc2.edu.vn>
2026-06-21 14:02:17 +0700

    fix(db): enable SSL for AWS RDS connection in production

--- config/config.js
+++ config/config.js
@@ -15,6 +15,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
