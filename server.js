8d938431166d552cbc580d879bb09b5cb3fac23c
Bui Dai Nghia <6451071050@st.utc2.edu.vn>
2026-06-22 17:50:40 +0700

    fix: CORS whitelist

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "http://localhost:3000"
+  "http://localhost:3000",
+  'https://plant-notebook.id.vn',
 ].filter(Boolean);
 
 app.use(cors({
